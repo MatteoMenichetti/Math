afd37a0c688372d807f2064b241fdfe1f6f6ceb2
matteo <matteo.menichetti@stud.unifi.it>
2023-10-28 11:49:10 +0200

    aggiunta prima struttura del progetto

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main(int argc, char ** argv) {
-
+    if (argc<2){
+        printf("Numero di argomenti insufficienti");
+        return -1;
+    }
+    int finderPid = fork();
+    if(finderPid==0)
+        execl("./finder", "finder");
+    execl("./server", "server");
     return 0;
 }
