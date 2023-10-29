df8232a032daad74631c6f751e249413c31d268f
matteo <matteo.menichetti@stud.unifi.it>
2023-10-29 18:28:22 +0100

    aggiunto definizione comportamento della socket per comunicare con il server

--- src/finder.c
+++ src/finder.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
+#include "../lib/common.h"
+
+int definesocket() {
+    int client_socket_fd;
+    if ((client_socket_fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
+        perror("P2: socket");
+        exit(EXIT_FAILURE);
+    }
+    return client_socket_fd;
+}
+
+void connecting(int client_socket_fd) {
+    struct sockaddr_un sock_server;
+    strcpy(sock_server.sun_path, SOCKPATH);
+    sock_server.sun_family = AF_UNIX;
+    while ((connect(client_socket_fd, (struct sockaddr *) &sock_server, sizeof(sock_server))) == -1) {
+        perror("P2: connect");
+        sleep(1);
+    }
+}
 
 int main(int argc, char ** argv){
+
     return 0;
 }
