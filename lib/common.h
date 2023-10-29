5df8e4bfedc0bf371ba60ad25998da60f459e5cd
matteo <matteo.menichetti@stud.unifi.it>
2023-10-29 19:05:43 +0100

    aggiunta linked list

--- lib/common.h
+++ lib/common.h
@@ -2,4 +2,10 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 #define SOCKPATH "../tmp/socket"
+
+struct numbers {int n; struct numbers *  next;};
