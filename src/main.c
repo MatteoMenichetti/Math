78fa8c3702dfae2306e086a279b114a9214b253b
matteo <matteo.menichetti@stud.unifi.it>
2023-10-29 18:19:33 +0100

    aggiunto comportamento (creazione server e finder con relativi input)

--- src/main.c
+++ src/main.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <signal.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char ** argv) {
     if (argc<2){
-        printf("Numero di argomenti insufficienti");
+        printf("main: numero di argomenti insufficienti");
         return -1;
     }
+    if (*argv[1]<2)
+        perror("main: input minore di 2");
     int finderPid = fork();
-    if(finderPid==0)
-        execl("./finder", "finder");
-    execl("./server", "server");
+    if(finderPid==0){
+        if(execl("./finder", "finder", NULL)==-1) {
+            printf("main: errore fork finder %d\n", errno);
+            exit(-1);
+        }
+    }else{
+        char * cFinderPid= malloc(sizeof(int));
+        sprintf(cFinderPid, "%d", finderPid);
+        if(execl("./server", "server", cFinderPid, argv[1])==-1) {
+            printf("main: errore fork server %d\n", errno);
+            kill(finderPid, SIGKILL);
+            exit(-1);
+        }
+    }
     return 0;
 }
