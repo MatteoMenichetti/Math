2a68901c32700f20841a0f20aecf8567cc7b78ca
matteo <matteo@matteo.com>
2024-01-23 21:13:51 +0100

    include primes_decision.h

--- src/finder.c
+++ src/finder.c
@@ -1,30 +1,8 @@
-#include <stdio.h>
-#include "../lib/common.h"
+#include "../lib/primes_decision.h"
 
-int definesocket() {
-    int client_socket_fd;
-    if ((client_socket_fd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1) {
-        perror("finder: socket");
-        exit(EXIT_FAILURE);
-    }
-    return client_socket_fd;
-}
-
-void connecting(int client_socket_fd) {
-    struct sockaddr_un sock_server;
-    strcpy(sock_server.sun_path, SOCKPATH);
-    sock_server.sun_family = AF_UNIX;
-    while ((connect(client_socket_fd, (struct sockaddr *) &sock_server, sizeof(sock_server))) == -1) {
-        perror("finder: connect");
-        sleep(1);
-    }
-}
 
 int main(int argc, char ** argv){
 
-    int fd = definesocket();
-
-    connecting(fd);
 
     return 0;
 }
--- src/server.c
+++ src/server.c
@@ -1,5 +1,4 @@
-#include "../lib/common.h"
-
+#include "../lib/primes_decision.h"
 
 int main(int argc, char ** argv){
 
