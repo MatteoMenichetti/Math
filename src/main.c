d4c53cf66d30248de518ca91df977c429a48d135
matteo <matteo@matteo.com>
2024-01-23 21:41:40 +0100

    swap common #include in common.h; reformat code; include start of log process; update and translate error message in english;

--- src/main.c
+++ src/main.c
@@ -1,31 +1,32 @@
-#include <stdio.h>
-#include <signal.h>
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <stdlib.h>
+#include "../lib/common.h"
 
-int main(int argc, char ** argv) {
-    if (argc<2){
-        printf("main: numero di argomenti insufficienti");
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("main: number of elements < 2");
         return -1;
     }
-    if (*argv[1]<2)
-        perror("main: input minore di 2");
+    if (*argv[1] < 2)
+        perror("main: prime limit number must be higher than 2");
     int finderPid = fork();
-    if(finderPid==0){
-        if(execl("./finder", "finder", NULL)==-1) {
-            printf("main: errore fork finder %d\n", errno);
-            exit(EXIT_FAILURE);
-        }
-    }else{
-        char * cFinderPid= (char*)malloc(sizeof(int));
-        sprintf(cFinderPid, "%d", finderPid);
-        if(execl("./server", "server", cFinderPid, argv[1], NULL)==-1) {
-            printf("main: errore fork server %d\n", errno);
-            kill(finderPid, SIGKILL);
+    if (finderPid == 0) {
+        if (execl("./finder", "finder", NULL) == -1) {
+            printf("main: error on finder fork %d\n", errno);
             exit(EXIT_FAILURE);
         }
     }
-    return 0;
+    if (fork() == 0) {
+        if (execl("./log", "log", NULL) == -1) {
+            printf("main: error on log fork (errno = %d)\n", errno);
+            exit(EXIT_FAILURE);
+        }
+    }
+    char *cFinderPid = (char *) malloc(sizeof(int));
+    sprintf(cFinderPid, "%d", finderPid);
+    if (execl("./server", "server", cFinderPid, argv[1], NULL) == -1) {
+        printf("main: error on server fork (errno = %d)\n", errno);
+        kill(finderPid, SIGKILL);
+        exit(EXIT_FAILURE);
+    }
+
+    return EXIT_SUCCESS;
 }
