1e8b0d626c7be1f9b3e38790c6109d6a38849c69
matteo <matteo@matteo.com>
2024-01-25 22:09:25 +0100

    first implementation, bug on reopen file

--- src/log.c
+++ src/log.c
@@ -1,5 +1,23 @@
 #include "../lib/log.h"
 
-int main(){
+int open_log(){
+    int fd = open(LOGPATH, O_CREAT|O_WRONLY|O_TRUNC);
+
+    if(fd==-1){
+        perror("log: open");
+        exit(EXIT_FAILURE);
+    }
+
+    if(write(fd, "log: start\n", sizeof("log: start\n"))<0){
+        perror("log: write on general.log");
+        exit(EXIT_FAILURE);
+    }
+
+    return fd;
+}
+
+int main(int argc, char** argv){
+    int fd = open_log();
+
     return EXIT_SUCCESS;
 }
