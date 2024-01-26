f7381da973d12c62c221489c19deb07879f4975a
matteo <matteo@matteo.com>
2024-01-26 21:59:23 +0100

    formatting

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@ int main(int argc, char **argv) {
         printf("main: prime limit number must be higher than 2");
         return EXIT_FAILURE;
     }
+
     int log_pid = fork();
     if (log_pid == 0) {
         if (execl("./log", "log", NULL) == -1) {
@@ -16,6 +17,7 @@ int main(int argc, char **argv) {
             exit(EXIT_FAILURE);
         }
     }
+
     int finder_pid = fork();
     if (finder_pid == 0) {
         if (execl("./finder", "finder", NULL) == -1) {
@@ -23,6 +25,7 @@ int main(int argc, char **argv) {
             exit(EXIT_FAILURE);
         }
     }
+
     char *cFinderPid = (char *) malloc(sizeof(int));
     sprintf(cFinderPid, "%d", finder_pid);
     if (execl("./server", "server", cFinderPid, argv[1], NULL) == -1) {
