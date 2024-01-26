f1a58ffd59b0f03077176124e092e8026c0e1ea8
matteo <matteo@matteo.com>
2024-01-26 22:01:35 +0100

    globalize fd_log & reformat code due to this action;
    define open_pipe (open pipe & return fd);

--- src/log.c
+++ src/log.c
@@ -1,28 +1,45 @@
 #include "../lib/log.h"
 
-ssize_t write_on_log(int fd, char *msg, int size) {
+int fd_log;
+
+ssize_t write_on_log(char *msg, int size) {
     ssize_t c_write = 0;
-    if ((c_write = write(fd, msg, size)) == -1)
+    if ((c_write = write(fd_log, msg, size)) == -1)
         perror("log: write on log");
 
     return c_write;
 }
 
-int open_log() {
-    int fd = open(LOGPATH, O_CREAT | O_WRONLY | O_TRUNC, S_IREAD + S_IWUSR + S_IROTH + S_IRGRP);
+void open_log() {
+    fd_log = open(LOGPATH, O_CREAT | O_WRONLY | O_TRUNC, S_IREAD + S_IWUSR + S_IROTH + S_IRGRP);
 
-    if (fd == -1) {
+    if (fd_log == -1) {
         perror("log: open");
         exit(EXIT_FAILURE);
     }
 
-    write_on_log(fd, "log: start\n", sizeof("log: start\n"));
+    write_on_log("log: start\n", sizeof("log: start\n"));
+}
 
+int open_pipe() {
+    unlink(LOGPIPE);
+    mknod(LOGPIPE, S_IFIFO + S_IREAD + S_IWGRP, 0);
+
+    int fd = open(LOGPIPE, O_RDONLY);
+    if (fd == -1) {
+        char *msg = (char *) calloc(sizeof("log: open %s failure") + sizeof(LOGPIPE), sizeof(char));
+        int size = sprintf(msg, "log: open %s failure", LOGPIPE);
+
+        write_on_log(msg, size);
+
+        perror(msg);
+    }
     return fd;
 }
 
 int main(int argc, char **argv) {
-    int fd_log = open_log();
+    open_log();
+    int fd_pipe = open_pipe();
 
     return EXIT_SUCCESS;
 }
