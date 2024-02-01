7f921527550e608b3b3133b22857856d64fffabe
matteo <matteo@matteo.com>
2024-02-01 21:36:39 +0100

    delete test

--- tests/log_test.cpp
+++ tests/log_test.cpp
@@ -1,14 +1,5 @@
 #include <gtest/gtest.h>
 
 extern "C" {
-#include "../src/log.c"
-}
-
-TEST(log_test, create_logfile){
-
-    unlink(LOGPATH);
-
-    open_log();
-
-    ASSERT_NE(fd_log, -1);
+#include "../lib/log.h"
 }
