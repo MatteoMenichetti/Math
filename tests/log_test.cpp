6ef24d6cfde7d53722556e0d85aa7e8363f9e26f
matteo <matteo@matteo.com>
2024-02-06 17:51:48 +0100

    add test open_pipe

--- tests/log_test.cpp
+++ tests/log_test.cpp
@@ -3,3 +3,26 @@
 extern "C" {
 #include "../lib/log.h"
 }
+
+TEST(LogTest, openPipe) {
+
+    open_log();
+
+    if(fork()==0){
+
+        int fd = open_pipe();
+
+        ASSERT_GT(fd, 0);
+
+        close(fd);
+
+        exit(EXIT_SUCCESS);
+    }
+
+    usleep(500);
+
+    if(open(LOGPIPE, O_WRONLY)==-1){
+        perror("test");
+        exit(EXIT_FAILURE);
+    }
+}
