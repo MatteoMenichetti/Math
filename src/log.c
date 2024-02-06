d8c426252ba1afd2b2ee59635d9cae5c06336a86
matteo <matteo@matteo.com>
2024-02-06 17:02:33 +0100

    add function read_log, this function read from pipe and write the log recived

--- src/log.c
+++ src/log.c
@@ -1,9 +1,28 @@
 #include "../lib/log.h"
 
+#define MSGSIZE 100
+
+void read_log(int fd) {
+    char *msg = (char *) calloc(MSGSIZE, sizeof(char)), *head = msg;
+
+    while (1) {
+        do {
+            if (read(fd, msg++, sizeof(char)) == 0)sleep(1);
+        }
+        while (strcmp(msg, "\n") == 0);
+
+        write_on_log(head, strlen(head) * sizeof(char));
+    }
+
+}
+
 int main(int argc, char **argv) {
+
     open_log();
 
     int fd_pipe = open_pipe();
+
+    read_log(fd_pipe);
 
     return EXIT_SUCCESS;
 }
